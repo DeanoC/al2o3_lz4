281fe288d8accc4902e2b2606ecdf99699407b9d
deano <me@deanoc.com>
2019-05-09 20:23:37 +0300

    Added HC and XXH64 to al2o3 api. Made original api private, added a compat.h to provide some of the original api

--- include/al2o3_lz4/compat.h
+++ include/al2o3_lz4/compat.h
@@ -1,18 +1,33 @@
 // BSD-2 part Licensed (full license and info at end of file)
-
 #pragma once
-#ifndef AL2O3_LZ4_LZ4_H
-#define AL2O3_LZ4_LZ4_H
+#ifndef AL2O3_LZ4_COMPAT_H
+#define AL2O3_LZ4_COMPAT_H
 
 #include "al2o3_platform/platform.h"
 
-AL2O3_EXTERN_C int LZ4_Compress(uint8_t const* src, uint8_t* dst, size_t srcSize, size_t dstCapacity);
-AL2O3_EXTERN_C int LZ4_CompressHigh (uint8_t const* src, uint8_t * dst, size_t srcSize, size_t dstCapacity, int compressionLevel);
-AL2O3_EXTERN_C int LZ4_Decompress(uint8_t const* src, uint8_t* dst, size_t compressedSize, size_t dstCapacity);
-AL2O3_EXTERN_C size_t LZ4_CompressionBoundFromInputSize(size_t inputSize);
-AL2O3_EXTERN_C uint64_t LZ4_XXHash(void const* src, size_t length, uint64_t seed);
+// these doesn't follow al2o3 naming conventions because its for anything that
+// wants the original lz4 api definations
 
-#endif // end AL2O3_LZ4_LZ4_H
+#define LZ4_VERSION_MAJOR    1    /* for breaking interface changes  */
+#define LZ4_VERSION_MINOR    9    /* for new (non-breaking) interface capabilities */
+#define LZ4_VERSION_RELEASE  1    /* for tweaks, bug-fixes, or development */
+#define LZ4_VERSION_NUMBER (LZ4_VERSION_MAJOR *100*100 + LZ4_VERSION_MINOR *100 + LZ4_VERSION_RELEASE)
+#define LZ4_LIB_VERSION LZ4_VERSION_MAJOR.LZ4_VERSION_MINOR.LZ4_VERSION_RELEASE
+#define LZ4_MAX_INPUT_SIZE        0x7E000000   /* 2 113 929 216 bytes */
+#define LZ4_COMPRESSBOUND(isize)  ((unsigned)(isize) > (unsigned)LZ4_MAX_INPUT_SIZE ? 0 : (isize) + ((isize)/255) + 16)
+
+AL2O3_EXTERN_C int LZ4_versionNumber (void);
+AL2O3_EXTERN_C const char* LZ4_versionString (void);
+
+AL2O3_EXTERN_C int LZ4_compress_default(char const* src, char* dst, int srcSize, int dstCapacity);
+AL2O3_EXTERN_C int LZ4_decompress_safe(char const* src, char* dst, int compressedSize, int dstCapacity);
+AL2O3_EXTERN_C size_t LZ4_compressBound(int inputSize);
+AL2O3_EXTERN_C int LZ4_compress_fast(char const* src, char* dst, int srcSize, int dstCapacity, int acceleration);
+AL2O3_EXTERN_C int LZ4_sizeofState(void);
+AL2O3_EXTERN_C int LZ4_compress_fast_extState (void* state, const char* src, char* dst, int srcSize, int dstCapacity, int acceleration);
+AL2O3_EXTERN_C int LZ4_decompress_safe_partial (const char* src, char* dst, int srcSize, int targetOutputSize, int dstCapacity);
+
+#endif // end AL2O3_LZ4_COMPAT_H
 /*
  Copyright (C) 2019-present, Dean Calver.
  Includes lz4 by Yann Collet - LZ4 source repository : https://github.com/lz4/lz4
--- src/lz4.c
+++ src/lz4.c
@@ -1,13 +1,21 @@
 #include "al2o3_lz4/lz4.h"
 #include "lib/lz4.h"
+#include "lib/lz4hc.h"
+#include "lib/xxhash.h"
 
 AL2O3_EXTERN_C int LZ4_Compress(uint8_t const* src, uint8_t* dst, size_t srcSize, size_t dstCapacity) {
 	return LZ4_compress_default(src, dst, (int)srcSize, (int)dstCapacity);
+}
+AL2O3_EXTERN_C int LZ4_CompressHigh (uint8_t const* src, uint8_t * dst, size_t srcSize, size_t dstCapacity, int compressionLevel) {
+	return LZ4_compress_HC(src, dst, (int)srcSize, (int)dstCapacity, compressionLevel );
 }
 AL2O3_EXTERN_C int LZ4_Decompress(uint8_t const* src, uint8_t* dst, size_t compressedSize, size_t dstCapacity) {
 	return LZ4_decompress_safe(src, dst, (int)compressedSize, (int)dstCapacity);
 }
 AL2O3_EXTERN_C size_t LZ4_CompressionBoundFromInputSize(size_t inputSize) {
 	return LZ4_compressBound((int)inputSize);
+}
+AL2O3_EXTERN_C uint64_t LZ4_XXHash(void const* src, size_t length, uint64_t seed) {
+	return XXH64(src, length, seed);
 }
 
