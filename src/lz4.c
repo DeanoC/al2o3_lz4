83a4a146c1150e684318a6bac43afc83f7b85c0c
Dean Calver <me@deanoc.com>
2019-05-17 13:03:21 +0300

    OS X now working :) Added central SET_MIN_VERSION macro

--- src/lz4.c
+++ src/lz4.c
@@ -4,13 +4,13 @@
 #include "lib/xxhash.h"
 
 AL2O3_EXTERN_C int LZ4_Compress(uint8_t const* src, uint8_t* dst, size_t srcSize, size_t dstCapacity) {
-	return LZ4_compress_default(src, dst, (int)srcSize, (int)dstCapacity);
+	return LZ4_compress_default((char const*)src, (char*)dst, (int)srcSize, (int)dstCapacity);
 }
 AL2O3_EXTERN_C int LZ4_CompressHigh (uint8_t const* src, uint8_t * dst, size_t srcSize, size_t dstCapacity, int compressionLevel) {
-	return LZ4_compress_HC(src, dst, (int)srcSize, (int)dstCapacity, compressionLevel );
+	return LZ4_compress_HC((char const*)src, (char*)dst, (int)srcSize, (int)dstCapacity, compressionLevel );
 }
 AL2O3_EXTERN_C int LZ4_Decompress(uint8_t const* src, uint8_t* dst, size_t compressedSize, size_t dstCapacity) {
-	return LZ4_decompress_safe(src, dst, (int)compressedSize, (int)dstCapacity);
+	return LZ4_decompress_safe((char const*)src, (char*)dst, (int)compressedSize, (int)dstCapacity);
 }
 AL2O3_EXTERN_C size_t LZ4_CompressionBoundFromInputSize(size_t inputSize) {
 	return LZ4_compressBound((int)inputSize);
